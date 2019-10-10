4b1a019d091db2be0a847969ab35dd369bb1d4b8
Humberto Alcocer <humbertowoody@gmail.com>
2019-10-09 23:16:57 -0500

    Update

--- Prácticas/Práctica-4/protocol.cpp
+++ Prácticas/Práctica-4/protocol.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstdio>
 #include <thread>
 #include <random>
 #include <algorithm>
-#include <pcap.h>
 #include <mutex>
+#include <fstream>
+#include <pcap.h>
 
 // ID_LENGTH
 #ifndef ID_LENGTH
 #define ID_LENGTH 8
 #endif
 
-// Mutexes
+/**
+ * Estas funciones generan un modo de impresión segura para
+ * arquitecturas multi-hilo.
+ */
+
+std::ostream &
+print_one(std::ostream &os)
+{
+  return os;
+}
+
+template <class A0, class... Args>
+std::ostream &
+print_one(std::ostream &os, const A0 &a0, const Args &... args)
+{
+  os << a0;
+  return print_one(os, args...);
+}
+
+template <class... Args>
+std::ostream &
+print(std::ostream &os, const Args &... args)
+{
+  return print_one(os, args...);
+}
+
+std::mutex &
+get_cout_mutex()
+{
+  static std::mutex m;
+  return m;
+}
+
+template <class... Args>
+std::ostream &
+print(const Args &... args)
+{
+  std::lock_guard<std::mutex> _(get_cout_mutex());
+  return print(std::cout, args...);
+}
+
+// Prototipo de función para imprimir de forma segura (multi-hilo)
+void safe_print(std::string);
 
 // Typedef de char_array.
 typedef std::vector<char> char_array;
@@ -30,14 +71,15 @@ void listen_data(pcap_if_t *, std::string);
 int main(int argc, char **argv)
 {
   // Variables locales.
-  pcap_t *fp;
-  pcap_t *adhandle;
-  pcap_if_t *d;
-  pcap_if_t *alldevs;
-  u_char packet[100];
-  char errbuf[PCAP_ERRBUF_SIZE];
-  int i;
-  int inum;
+  pcap_t *fp;                    // El handle del dispositivo a usar para operaciones de captura y escucha.
+  pcap_if_t *d;                  // El dispositivo a usar.
+  pcap_if_t *alldevs;            // Todos los dispositivos de captura detectados.
+  u_char packet[100];            // El paquete a enviar.
+  char errbuf[PCAP_ERRBUF_SIZE]; // Buffer para mensajes de error.
+  int i;                         // Variable para operaciones de ciclos.
+  int inum;                      // Variable con el número de la interfaz a usar.
+  std::string filename;          // El nombre del archivo a enviar.
+  std::ifstream file;            // El archivo a leer.
 
   // Set de caracteres a usar para el ID.
   const auto ch_set = charset();
@@ -70,9 +112,9 @@ int main(int argc, char **argv)
 
   // Imprimir la lista.
   std::cout << "Lista de dispositivos disponibles:" << std::endl;
-  for (d = alldevs, i = 0; d; d = d->next, ++i)
+  for (d = alldevs, i = 0; d; d = d->next)
   {
-    std::cout << i << ") " << d->name;
+    std::cout << ++i << ") " << d->name;
     if (d->description)
     {
 
@@ -101,20 +143,59 @@ int main(int argc, char **argv)
   for (d = alldevs, i = 0; i < inum - 1; d = d->next, i++)
     ;
 
-  // Iniciar hilo de escucha.
-  std::thread(listen_data, d, id).detach();
-
-  /* Open the output device */
-  if ((fp = (pcap_t *)pcap_open_live(d->name, // name of the device  //argv[1]
-                                     100,     // portion of the packet to capture (only the first 100 bytes)
-                                     1,       //PCAP_OPENFLAG_PROMISCUOUS,  // promiscuous mode
-                                     1000,    // read timeout
-                                     errbuf   // error buffer
+  // Abrir dispositivo de red.
+  if ((fp = (pcap_t *)pcap_open_live(d->name, // Nombre del dispositivo.
+                                     1000,    // Porción del paquete a capturar (solo los primeros 100 bytes)
+                                     1,       // Modo promiscuo.
+                                     1000,    // Timeout de lectura.
+                                     errbuf   // Buffer para almacenar errores.
                                      )) == NULL)
   {
-    fprintf(stderr, "\nUnable to open the adapter. %s is not supported by WinPcap\n", argv[1]);
+    std::cerr << "Ha sido imposible abrir el adaptador de red, error: " << errbuf << std::endl;
     return -1;
   }
+
+  /**
+   * Hasta este punto, usamos std::cout porque no habíamos iniciado otra operación
+   * multi-hilo. A partir de aquí, usamos la función print() que es segura en este
+   * contexto de operaciones multi-hilo.
+   */
+
+  // Iniciar hilo de escucha.
+  std::thread(listen_data, std::ref(fp), id).detach();
+
+  // Informar al usuario.
+  print("Escuchando paquetes enviados a este dispositivo con el ID: ", id, " .\n");
+  print("A partir de este momento, cualquier paquete recibido será procesado y almacenado\n");
+  print("en caso de ser dirigido a este dispositivo.\n");
+  print("(Se informará sobre segmentos recibidos)\n");
+
+  // Preguntar al usuario por el nombre del archivo a enviar.
+  print("Escriba el nombre del archivo a enviar: ");
+  std::cin >> filename;
+
+  // Confirmar el nombre del archivo con el usuario.
+  print("Se leerá el archivo: ", filename, "\n");
+
+  // Leer archivo.
+  file.open(filename, std::ios::in | std::ios::binary | std::ios::ate);
+  if (!file.is_open())
+  {
+    print("Ha ocurrido un error abriendo el archivo. Verifique la información e inténtalo más tarde.\n");
+    return -1;
+  }
+
+  // Obtenemos el tamaño del archivo.
+  file.seekg(0, std::ios::end);
+
+  print("El archivo mide: ", file.tellg(), " bytes.\n");
+
+  // Iniciamos en el primer elemento del archivo.
+  file.seekg(0, std::ios::beg);
+
+  // Dividir el archivo en pedazos.
+
+  // Enviar cada pedazo y esperar confirmación.
 
   /* Supposing to be on ethernet, set mac destination to 1:1:1:1:1:1 */
   packet[0] = 1;
@@ -152,7 +233,7 @@ int main(int argc, char **argv)
 }
 
 /**
- * The Charset to be used for ID generation.
+ * El conjunto de caracteres a usar para la generación del ID.
  */
 char_array charset()
 {
@@ -172,7 +253,7 @@ char_array charset()
 };
 
 /**
- * Function to generate a random string of fixed length.
+ * Función que genera un string aleatorio.
  */
 std::string random_string(size_t length, std::function<char(void)> rand_char)
 {
@@ -181,9 +262,18 @@ std::string random_string(size_t length, std::function<char(void)> rand_char)
   return str;
 }
 
-// Listener thread function prototype.
+/**
+ * Función que escucha en el dispositivo especificado para captura y realiza
+ * el guardado de los segmentos del archivo enviado.
+ */
 void listen_data(pcap_if_t *d, std::string id)
 {
-  std::cout << "ID: " << id << std::endl;
-  std::cout << "Nombre: " << d->name << std::endl;
+  print("ID: ", id, '\n');
+  print("Nombre: ", d->name, '\n');
+
+  // Compilar filtro
+
+  // setear filtro
+
+  // abrir captura infinita
 }
