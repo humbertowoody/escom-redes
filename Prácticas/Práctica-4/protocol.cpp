8013ee55312651263e2db0c2b2232c57b7fbd389
Humberto Alcocer <humbertowoody@gmail.com>
2019-10-15 15:17:43 -0500

    Avance en práctica y adición de archivo de prueba para transmisión

--- Prácticas/Práctica-4/protocol.cpp
+++ Prácticas/Práctica-4/protocol.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <fstream>
 #include <pcap.h>
+#include <signal.h>
 
 // ID_LENGTH
 #ifndef ID_LENGTH
@@ -52,8 +53,11 @@ print(const Args &... args)
   return print(std::cout, args...);
 }
 
-// Prototipo de función para imprimir de forma segura (multi-hilo)
-void safe_print(std::string);
+// Mutexes para operaciones multi-hilo.
+std::mutex sigint_mtx;
+
+// Banderas globales.
+bool shutdown_requested;
 
 // Typedef de char_array.
 typedef std::vector<char> char_array;
@@ -65,7 +69,16 @@ char_array charset();
 std::string random_string(size_t, std::function<char(void)>);
 
 // Prototipo de función para escuchar archivos entrantes.
-void listen_data(pcap_if_t *, std::string);
+void listen_data(pcap_t *, std::string);
+
+// Prototipo de función para handlear apagado seguro.
+void shutdown_handler(pcap_t *);
+
+// Prototipo de función para procesar los paquetes entrantes.
+void incoming_packet_handler(unsigned char *, const struct pcap_pkthdr *, const unsigned char *);
+
+// Prototipo de handler de señal de fin de programa (ctrl+c)
+void signal_handler(int);
 
 // Función principal.
 int main(int argc, char **argv)
@@ -80,6 +93,14 @@ int main(int argc, char **argv)
   int inum;                      // Variable con el número de la interfaz a usar.
   std::string filename;          // El nombre del archivo a enviar.
   std::ifstream file;            // El archivo a leer.
+  struct sigaction sigIntHandler;
+
+  sigIntHandler.sa_handler = signal_handler;
+  sigemptyset(&sigIntHandler.sa_mask);
+  sigIntHandler.sa_flags = 0;
+
+  // Enlazar la señal con el handler.
+  sigaction(SIGINT, &sigIntHandler, NULL);
 
   // Set de caracteres a usar para el ID.
   const auto ch_set = charset();
@@ -162,7 +183,10 @@ int main(int argc, char **argv)
    */
 
   // Iniciar hilo de escucha.
-  std::thread(listen_data, d, id).detach();
+  std::thread listener(listen_data, fp, id);
+
+  // Iniciar hilo de apagado seguro.
+  std::thread shuwtdown_thread(shutdown_handler, fp);
 
   // Informar al usuario.
   print("Escuchando paquetes enviados a este dispositivo con el ID: ", id, " .\n");
@@ -229,6 +253,15 @@ int main(int argc, char **argv)
     return -1;
   }
 
+  // Informar al usuario sobre el proceso de shutdown.
+  print("Esperando a cerrar hilo de escucha...\n");
+
+  // Finalizar hilo de escucha.
+  listener.join();
+
+  // Informar al usuario sobre el final de escucha.
+  print("Se ha cerrado correctamente el hilo de escucha.\n");
+
   return 0;
 }
 
@@ -266,14 +299,74 @@ std::string random_string(size_t length, std::function<char(void)> rand_char)
  * Función que escucha en el dispositivo especificado para captura y realiza
  * el guardado de los segmentos del archivo enviado.
  */
-void listen_data(pcap_if_t *d, std::string id)
+void listen_data(pcap_t *d, std::string id)
 {
-  print("ID: ", id, '\n');
-  print("Nombre: ", d->name, '\n');
+  // Variables locales.
+  int number_of_packets = 0;
 
-  // Compilar filtro
+  // Imprimir datos.
+  print("(Escuchando conexiones entrantes...)\n");
 
-  // setear filtro
+  // Abrir captura infinita
+  number_of_packets = pcap_loop(d, -1, incoming_packet_handler, NULL);
 
-  // abrir captura infinita
+  // Detectar si hubo errores en la captura.
+  if (number_of_packets == -2)
+  {
+    print("Se cerró correctamente la lectura de paquetes.\n");
+  }
+  else if (number_of_packets == -1)
+  {
+    print("Hubo un error leyendo paquetes: ", pcap_geterr(d), "\n");
+  }
+
+  // Imprimir el número de paquetes capturados.
+  print("Se leyeron ", number_of_packets, " paquetes correctamente.");
+}
+
+/**
+ * Función para procesar cada paquete que llega al dispositovo de red
+ * seleccionado. Ofrece opciones de recuperación y de reenvío.
+ */
+void incoming_packet_handler(unsigned char *param, const struct pcap_pkthdr *header, const unsigned char *pkt_data)
+{
+  if (pkt_data[12] == 0x16 && pkt_data[13] == 0x01)
+  {
+    print("¡Llegó un paquete nuestro!\n");
+  }
+}
+
+/**
+ * Función para procesar señales SIGINT para finalizar el programa.
+ */
+void signal_handler(int s)
+{
+  print("Se ha recibido una señal de finalización de ejecución.\n");
+  sigint_mtx.lock();
+  shutdown_requested = true;
+  sigint_mtx.unlock();
+}
+
+/**
+ * Función que verifica si se ha solicitado un SIGTERM del programa
+ * para finalizar el ciclo de escucha.
+ */
+void shutdown_handler(pcap_t *d)
+{
+  // Mutexes para proteger el acceso a la variable.
+  while (true)
+  {
+
+    sigint_mtx.lock();
+    if (shutdown_requested)
+    {
+      // Finalizar ciclo de escucha de paquetes.
+      pcap_breakloop(d);
+      // Cambiar variable.
+      shutdown_requested = false;
+      // Salir del ciclo.
+      break;
+    }
+    sigint_mtx.unlock();
+  }
 }
